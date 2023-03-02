8147cf29b4057728330738a2a2101622ecfce966
tiagoamp <tiagoamp@gmail.com>
2023-03-02 20:29:10 -0300

    Finaliza exercicio

--- src/components/Selecao/index.js
+++ src/components/Selecao/index.js
@@ -1,10 +1,24 @@
+import PropTypes from 'prop-types';
 import './styles.css'
 
-export default function Selecao( props ) {
-
-    console.log(props);
-
+function Selecao( { sigla, nome, grupo } ) {
+    // console.log(props);
+    // props.sigla
+    // props.nome
+    // props.grupo
   return (
-    <div> selecao </div>
+    <div className='linha-selecao'>
+        <span>{nome}</span>
+        <span>{sigla}</span>
+        <span>{grupo}</span> 
+    </div>
   )
 }
+
+Selecao.propTypes = {
+    nome: PropTypes.string.isRequired, 
+    sigla: PropTypes.string.isRequired,
+    grupo: PropTypes.string.isRequired
+}
+
+export default Selecao;
